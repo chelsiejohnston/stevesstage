6c97b6f3505f1bbc42757ab1eada45ace0b90f3f
Chelsie Johnston <chelsiesjohnston@gmail.com>
2016-09-07 11:14:30 -0500

    Devel; two col layout for /lessons

---
+++ sites/all/themes/stevesstage/ds_layouts/lesson_two_column/lesson-two-column.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file
+ * Display Suite Lesson Two Column template.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * Regions:
+ *
+ * - $left: Rendered content for the "Left" region.
+ * - $left_classes: String of classes that can be used to style the "Left" region.
+ *
+ * - $right: Rendered content for the "Right" region.
+ * - $right_classes: String of classes that can be used to style the "Right" region.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+
+  <!-- Needed to activate contextual links -->
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+<<?php print $top_wrapper; ?> class="ds-top<?php print $top_classes; ?>">
+<?php print $top; ?>
+</<?php print $top_wrapper; ?>>
+
+    <<?php print $left_wrapper; ?> class="ds-left<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </<?php print $left_wrapper; ?>>
+
+    <<?php print $right_wrapper; ?> class="ds-right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </<?php print $right_wrapper; ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
